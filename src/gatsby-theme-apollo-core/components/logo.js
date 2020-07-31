4bdd4f2e409d05ca90be8f3767f80e9dcaf3ef6c
Tan Nguyen Le <nguyenletan@gmail.com>
2020-07-31 17:59:35 +1000

    Initial commit

---
+++ src/gatsby-theme-apollo-core/components/logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logoImg from '../../assets/icons/NashTech-logo-new.svg'
+
+export default function Logo () {
+  return <h4 style={{ marginBottom: 0 }}>
+    <img src={logoImg}
+         width="70"
+         height="70"
+         alt="NashTech"/>
+  </h4>
+}
